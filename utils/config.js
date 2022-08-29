81c2599f33b8cd63f451707bc41dd81b7f7f4c98
Shelly <shellyniazov11@gmail.com>
2022-08-29 19:15:13 +0300

    server

---
+++ utils/config.js
@@ -0,0 +1,14 @@
+
+// Sql יצירת התחברות עם ה 
+module.exports = {
+    db: {
+        user: process.env.DB_User,
+        password: process.env.DB_Password,
+        server: process.env.DB_Server,
+        database: process.env.DB_Data_Base,
+        options: {
+            enableArithAbort: true,
+            trustServerCertificate: true
+        }
+    },
+}
